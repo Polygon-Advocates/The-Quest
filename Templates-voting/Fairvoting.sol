4ea371f748103b5ec3ddd639bc49c7faece60e20
Ananyaagupta <sonyguptashikohabad@gmail.com>
2023-05-11 22:29:02 +0530

    basic codes

---
+++ Templates-voting/Fairvoting.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT  
+pragma solidity ^0.8.0;
+
+contract Fairvoting {
+    string public question = "who is cooler";
+    string public optionA = "superman";
+    string public optionB = "spiderman";
+    uint public optionAVotes;
+    uint public optionBVotes;
+
+    mapping(address => bool) public hasVoted;
+    mapping(address => string) public userVotes;
+
+    function voteForOptionA() public {
+        require(!hasVoted[msg.sender], "You have already voted.");
+        optionAVotes++;
+        hasVoted[msg.sender] = true;
+        userVotes[msg.sender] = optionA;
+    }
+
+    function voteForOptionB() public {
+        require(!hasVoted[msg.sender], "You have already voted.");
+        optionBVotes++;
+        hasVoted[msg.sender] = true;
+        userVotes[msg.sender] = optionB;
+    }
+
+}
